eecaed6a7307c264b1a6c4c428712a11212e54bf
Ofer Koren <koreno@gmail.com>
2023-01-22 18:56:28 +0200

    bug fix on missing stdout/err attributes on ProcessExecutionError object

--- plumbum/commands/processes.py
+++ plumbum/commands/processes.py
@@ -155,6 +155,8 @@ class ProcessExecutionError(OSError):
         if isinstance(stdout, list):
             assert isinstance(stderr, list)
             self.all_output += self._format_lines(heapq.merge(stdout, stderr))
+            self.stdout = "\n".join(line for *_, line in stdout)
+            self.stderr = "\n".join(line for *_, line in stderr)
         else:
             if isinstance(stdout, bytes):
                 stdout = ascii(stdout)
